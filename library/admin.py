92be6df6529cbeff8f4b93591fbbab437c58b8ad
Ramunas P <ramunas.platukis@gmail.com>
2025-01-31 14:09:54 +0200

    P02 Genre, BookInstance modeliai
    Sukūrus modelius ar padarę juose pakeitimus, visada vykdome:
    python manage.py makemigrations
    python manage.py migrate
    Taip pat, padarę pakeitimus admin faile, atsidarome admin svetainę
    ir papildome naujas lenteles duomenimis

--- library/admin.py
+++ library/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Author, Book
+from .models import Author, Book, BookInstance, Genre
 
 admin.site.register(Author)
 admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Genre)
--- library/models.py
+++ library/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import uuid
+
 
 class Author(models.Model):
     """
@@ -11,6 +13,27 @@ class Author(models.Model):
     def __str__(self):
         return f'{self.last_name} {self.first_name}'
 
+    class Meta:
+        """
+        Globalūs nuostatai Author modelio
+        lentelei.
+        ordering - rikiavimas
+        """
+        ordering = ('last_name', 'first_name')
+
+
+class Genre(models.Model):
+    """
+    Žanrų lentelė, bus prijungta
+    many-to-many su Book
+    help_text - admino svetainėje
+    rodomas pagalbos tekstas prie šio lauko
+    """
+    name = models.CharField('Žanrai', max_length=15, help_text="Įveskite knygos žanrus")
+
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Book(models.Model):
     """
@@ -21,6 +44,37 @@ class Book(models.Model):
     summary = models.TextField('Aprašymas', max_length=1300)
     isbn = models.CharField('ISBN', max_length=13)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    genres = models.ManyToManyField(Genre)
 
     def __str__(self):
         return f"{self.title}"
+
+
+class BookInstance(models.Model):
+    """
+    Knygos konkretus egzempliorius išduodamas
+    skaitytojui. id sudarysime uuid tipo
+    pvz db195443-731e-4da7-9160-32928854fd87
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    due_back = models.DateField('Bus prieinama', null=True, blank=True)  # blank - dirba su Django forms
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    # statusų rinkinys, (statuso kodas, pilnas statuso pavadinimas)
+    LOAN_STATUS = (
+        ('a', 'Administruojamas'),
+        ('p', 'Paimta'),
+        ('g', 'Galima paimti'),
+        ('r', 'Rezervuota')
+    )
+
+    # pats lentelės stulpelis, kuris naudos statusų rinkinį
+    status = models.CharField('Statusas',
+                              max_length=1,
+                              choices=LOAN_STATUS,
+                              default='a',
+                              blank=True,
+                              help_text='Knygos egzemplioriaus statusas')
+
+    def __str__(self):
+        return f'{self.id} {self.status} {self.due_back} {self.book} {self.book.author}'
