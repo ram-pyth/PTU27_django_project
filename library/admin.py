7a228ddb56bc7b838389523caf6aafe45f66cf4b
Ramunas P <ramunas.platukis@gmail.com>
2025-02-03 14:43:11 +0200

    P03 Prie vieno book viewso pridėtas to book kopijų(book instance) sąrašas eilutėmis

--- library/admin.py
+++ library/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from .models import Author, Book, BookInstance, Genre
 
 
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0  # automatiškai kuriamų papildomų eilučių skaičius
+
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'isbn', 'display_genres')
     search_fields = ('title', 'author__last_name')  # FK__tėvinėslaukas
+    inlines = (BookInstanceInline,)
+
 
 class BookInstanceAdmin(admin.ModelAdmin):
     list_display = ('book', 'id', 'due_back', 'status')
