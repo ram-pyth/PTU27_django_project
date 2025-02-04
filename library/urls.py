7d2250e2eeec4c1c8b2c9fb2ee1f16cc8602e38c
Ramunas P <ramunas.platukis@gmail.com>
2025-02-04 14:30:53 +0200

    P05 Pridetas vienos knygos viewsas, sulinkinta
    Sukurtas vienos knygos viewsas class pagrindu, knygų listviewsas sulinkintas su individualiais kiekvienos knygos viewsais

--- library/urls.py
+++ library/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index_nm'),
     path('authors/', views.get_authors, name='authors-all'),
     path('authors/<int:author_id>', views.get_one_author, name='author-one'),
-    path('books/', views.BookListView.as_view(), name='books-all')
+    path('books/', views.BookListView.as_view(), name='books-all'),
+    path('books/<int:pk>', views.BookDetailView.as_view(), name='book-one'),
+
 ]
--- library/views.py
+++ library/views.py
@@ -40,3 +40,9 @@ class BookListView(generic.ListView):
     model = Book
     context_object_name = 'book_list'  # book_list - standartinis kintamojo template pavadinimas,sukuriamas django
     template_name = 'books.html'
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    context_object_name = 'book'  # book - standartinis kintamojo template pavadinimas,sukuriamas django
+    template_name = 'book.html'
