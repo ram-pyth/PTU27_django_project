b2144ff4e0a1e927274136bc412091c676328b05
Ramunas P <ramunas.platukis@gmail.com>
2025-02-11 16:03:06 +0200

    P10 Crispy forms
    pip install crispy-bootstrap4==2024.1
    pip installdjango-crispy-forms==2.3
    prijungta crispy biblioteka, gražesniems laukams formose
    prie user-books šablono prijungtas vartotojo avataras

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'library.apps.LibraryConfig',
     'tinymce',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -168,3 +170,5 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': True,
 }
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
