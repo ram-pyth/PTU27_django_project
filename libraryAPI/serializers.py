f15cba18059db45e295dd940a9dcf89e8de63b2b
Ramunas P <ramunas.platukis@gmail.com>
2025-02-27 17:25:30 +0200

    P13 Api BookSerializer su žanrais

--- libraryAPI/serializers.py
+++ libraryAPI/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from library.models import Author, Book
+from library.models import Author, Book, Genre
 
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -9,6 +9,14 @@ class AuthorSerializer(serializers.ModelSerializer):
 
 
 class BookSerializer(serializers.ModelSerializer):
+    author = serializers.CharField(source='author.__str__', read_only=True)
+    genres = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+
     class Meta:
         model = Book
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ('title', 'author', 'genres')
